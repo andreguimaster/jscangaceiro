cce15155718737d321699689ad080ff5b8c02bdb
Horácio Dias <horacio.dias92@gmail.com>
2017-11-19 20:29:47 -0200

    Adicionando tratativas de exceptions no client javascript

--- client/app/ui/converters/DateConverter.js
+++ client/app/ui/converters/DateConverter.js
@@ -10,7 +10,7 @@ class DateConverter {
 
     static paraData(texto) {
         if(!/^\d{2}\/\d{2}\/\d{4}$/.test(texto)){
-            throw new Error('Deve estar no formato dd/mm/aaaa');
+            throw new DataInvalidaException('Deve estar no formato dd/mm/aaaa');
         }
         return new Date(...texto.split('/')
         .reverse()
