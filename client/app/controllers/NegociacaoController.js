85325ca0b98b94f6e77b7d9a3f6dfe1c618e49db
Horácio Dias <horacio.dias92@gmail.com>
2017-11-19 19:57:44 -0200

    Iniciando consumo da api com xhr

--- client/app/controllers/NegociacaoController.js
+++ client/app/controllers/NegociacaoController.js
@@ -54,4 +54,27 @@ class NegociacaoController {
         this._negociacoes.apaga();
         this._mensagem.texto = 'Negociações apagadas com sucesso';
     }
+
+    importaNegociacoes () {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', 'negociacoes/semana');
+        xhr.onreadystatechange = () => {
+            if(xhr.readyState == 4) {
+                if (xhr.status == 200){
+                    console.log('Obtendo as negociações do servidor');
+                    JSON.parse(xhr.responseText)
+                    .map(objeto => new Negociacao(
+                        new Date(objeto.data),
+                        objeto.quantidade,
+                        objeto.valor
+                        )
+                    ).forEach(negociacao => this._negociacoes.adiciona(negociacao));
+                    this._mensagem.texto = 'Negociações importadas com sucesso!'
+                } else {
+                    this._mensagem.texto = 'Não foi possível obter as negociações da semana';
+                }
+            }
+        };
+        xhr.send();
+    }
 }
