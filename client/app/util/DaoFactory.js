b275048a635ffe7ae04a91b3134105093394371e
Horácio Dias <horacio.dias@yahoo.com>
2017-11-29 00:19:06 -0200

    Criando o NegociacaoDao e o DaoFactory

---
+++ client/app/util/DaoFactory.js
@@ -0,0 +1,12 @@
+class DaoFactory {
+    
+    constructor() {
+        throw new Error('Não é possível criar instâncias dessa classe');
+    }
+
+    static getNegociacaoDao() {
+        return ConnectionFactory
+            .getConnection()
+            .then(conn => new NegociacaoDao(conn));
+    }
+}
