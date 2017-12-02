fdd946077f7ba8c4a61fef78baa80dd1999ab2f2
Horácio Dias <horacio.dias@yahoo.com>
2017-12-02 13:52:10 -0200

    Passando a utilizar a api fetch e função de obrigatoriedade como parâmetro default

--- client/app-src/app.js
+++ client/app-src/app.js
@@ -9,3 +9,24 @@ $('#botao-apaga')
     .addEventListener('click', controller.apaga.bind(controller));
 $('#botao-importa')
     .addEventListener('click',controller.importaNegociacoes.bind(controller));
+
+/* Exemplo de um POST */
+/*
+import { Negociacao } from './domain/index.js';
+
+const negociacao = new Negociacao(new Date(), 1, 200);
+const headers = new Headers();
+headers.set('Content-Type', 'application/json');
+const body = JSON.stringify(negociacao);
+const method = 'POST';
+
+const config = {
+    method,
+    headers,
+    body
+};
+
+fetch('/negociacoes', config)
+    .then((res)=>res.json())
+    .then((dados)=>console.log(dados));
+*/
